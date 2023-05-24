6be5373cb687595fbf0c975afaedb943432a9e9e
Sandra Rehnstrom <sandra.rehnstrom@chasacademy.se>
2023-05-24 15:46:49 +0200

    min och nejis version

--- chatlabs/src/pages/db-4.tsx
+++ chatlabs/src/pages/db-4.tsx
@@ -9,6 +9,7 @@ function DashBoard4(props: any) {
   const { id } = useParams();
   const [character, setCharacter] = useState<{ name: string, backstory: string } | null>(null);
   const location = useLocation();
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     const fetchCharacter = async () => {
@@ -30,6 +31,22 @@ function DashBoard4(props: any) {
     
     fetchCharacter();
   }, [id]);
+
+  async function updateCharacter() {
+    await fetch("http://localhost:4000/edit/645c1385353c806b4d791675/0", {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name: "Tom", backstory: "His family was murderd", traits: "Angry killer"}),
+    });
+  }
+  
+  const deleteCharacter = async () => {
+    const response = await fetch("http://localhost:4000/delete/645c1385353c806b4d791675/0", {method: "delete"});
+    const res = await response.json();
+    console.log(res);
+  }
 
   if (!character) {
     return <div className='text-violet-500'>Loading...</div>;
@@ -82,11 +99,11 @@ function DashBoard4(props: any) {
            <div className="flex justify-between mt">
             <button className="px-7 py-2 bg-zinc-600 text-white font-semibold rounded-lg uppercase">Back</button>
             <div>
-              <button className="px-4 py-2  bg-zinc-600 text-white font-semibold rounded-lg mr-2">
+              <button onClick={() => setShowForm(!showForm)} className="px-4 py-2  bg-zinc-600 text-white font-semibold rounded-lg mr-2">
               <PencilIcon className="h-5 w-5" /> {/* Penna Icon */}
               </button>
 
-              <button className="px-4 py-2  bg-zinc-600 text-white font-semibold rounded-lg">
+              <button onClick={() => deleteCharacter()} className="px-4 py-2  bg-zinc-600 text-white font-semibold rounded-lg">
               <TrashIcon className="h-5 w-5 " /> {/* Papperskorg Icon */}
 
               </button>
@@ -94,6 +111,46 @@ function DashBoard4(props: any) {
               </div>
               </div>
               </div>
+
+              <div className="flex justify-center mt-10 pt-10 mb-10">
+          <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl shadow-lg shadow-violet-500/50 hover:shadow-violet-500/100 transition duration-300">
+            { showForm &&
+              <form className=" bg-black rounded-xl shadow-lg p-10">
+                <h2 className="text-xl text-violet-500 font-semibold mb-20 mt-8 uppercase text-center">Edit Character</h2>
+
+                {/* Input fält start*/}
+                <div className="mb-2">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Name
+                  </label>
+                  <input className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="" />
+                </div>
+                <div className="mb-2">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Backstory
+                  </label>
+                  <input className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-20" id="username" type="text" placeholder="" />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Traits
+                  </label>
+                  <input className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-20" id="username" type="text" placeholder="" />
+                </div>
+                {/* Input fält Stop*/}
+
+                {/* button start */}
+                <div className="flex items-center justify-between">
+                  <button onClick={() => updateCharacter()} className="bg-zinc-600 hover:bg-zinc-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline mb-10" type="button">
+                    EDIT
+                  </button>
+                </div>
+                {/* button end */}
+
+              </form>
+            }
+          </div>
+        </div>
               </>
               );
             }
