15cfb45c436ea8e4d04ff901834b08ff379543f5
Camilla Gustafsson <camilla.gustafsson@chasacademy.com>
2023-05-25 18:30:18 +0200

    updated link

--- chatlabs/src/components/CharacterForm.tsx
+++ chatlabs/src/components/CharacterForm.tsx
@@ -1,39 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export interface CharacterFormData {
   name: string;
   backstory: string;
+  traits: string;
 }
 
 interface CharacterFormProps {
   name: string;
   backstory: string;
+  traits: string;
   onNameChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onBackstoryChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onTraitsChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onSubmit: (formData: CharacterFormData) => Promise<void>;
 }
 
 const CharacterForm: React.FC<CharacterFormProps> = ({
   name,
   backstory,
+  traits,
   onNameChange,
   onBackstoryChange,
+  onTraitsChange,
   onSubmit,
 }) => {
+  const navigate = useNavigate();
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
     await onSubmit({
       name: name.trim(),
       backstory: backstory.trim(),
+      traits: traits.trim(),
     });
+
+    // Navigera till en annan rutt efter att formuläret har skickats
+    navigate('/Dashboard'); // Ersätt '/ny-rutt' med den faktiska rutten du vill navigera till
   };
 
   return (
     <form onSubmit={handleSubmit} className="bg-black rounded-xl shadow-lg p-10">
       <h2 className="text-xl text-violet-500 font-semibold mb-20 mt-8 uppercase text-center">Create Character</h2>
 
-      <div className="mb-2">
+      <div className="field-wrapper">
         <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
           Name
         </label>
@@ -43,16 +54,16 @@ const CharacterForm: React.FC<CharacterFormProps> = ({
           type="text"
           placeholder=""
           value={name}
-          onChange={onNameChange} // Använd onNameChange här för att binda händelsehanteraren
+          onChange={onNameChange}
         />
       </div>
 
-      <div className="mb-4">
+      <div className="field-wrapper">
         <label htmlFor="backstory" className="block text-gray-700 text-sm font-bold mb-2">
           Backstory
         </label>
         <input
-          className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-20"
+          className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-3"
           id="backstory"
           type="text"
           placeholder=""
@@ -61,14 +72,28 @@ const CharacterForm: React.FC<CharacterFormProps> = ({
         />
       </div>
 
+      <div className="field-wrapper">
+        <label htmlFor="traits" className="block text-gray-700 text-sm font-bold mb-2">
+          Traits
+        </label>
+        <input
+          className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-20"
+          id="traits"
+          type="text"
+          placeholder=""
+          value={traits}
+          onChange={onTraitsChange}
+        />
+      </div>
+
       <div className="flex items-center justify-between">
-        <Link
-          to="/Dashboard"
+        <button
+          type="submit"
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline mb-10"
         >
           Submit
-        </Link>
-        
+        </button>
+
         <Link
           to="/"
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline mb-10"
--- chatlabs/src/pages/db-2.tsx
+++ chatlabs/src/pages/db-2.tsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 import CharacterForm, { CharacterFormData } from '../components/CharacterForm';
 
-const Dashboard: React.FC = () => {
+const Dashboard2: React.FC = () => {
   const [response, setResponse] = useState('');
-  
+  const [name, setName] = useState('');
+  const [backstory, setBackstory] = useState('');
+  const [traits, setTraits] = useState('');
+
+  const navigate = useNavigate(); // Hämta navigeringsfunktionen
 
   const handleSubmit = async (formData: CharacterFormData): Promise<void> => {
     try {
-      const { name, backstory } = formData;
-      const response = await fetch(`http://localhost:4000/createuser/645c1385353c806b4d791675`, {
-        method: 'POST',
+      const { name, backstory, traits } = formData;
+      const response = await fetch(`http://localhost:4000/create/645c1385353c806b4d791675`, {
+        method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           name,
           backstory,
+          traits,
         }),
       });
 
@@ -26,14 +32,14 @@ const Dashboard: React.FC = () => {
       // Clear the form fields
       setName('');
       setBackstory('');
+      setTraits('');
+
+      navigate('/dashboard'); // Navigera till önskad rutt efter att formuläret har skickats
     } catch (error) {
       console.error(error);
       setResponse('An error occurred. Please try again.');
     }
   };
-
-  const [name, setName] = useState('');
-  const [backstory, setBackstory] = useState('');
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setName(event.target.value);
@@ -41,6 +47,10 @@ const Dashboard: React.FC = () => {
 
   const handleBackstoryChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setBackstory(event.target.value);
+  };
+
+  const handleTraitsChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setTraits(event.target.value);
   };
 
   return (
@@ -51,8 +61,10 @@ const Dashboard: React.FC = () => {
           <CharacterForm
             name={name}
             backstory={backstory}
+            traits={traits}
             onNameChange={handleNameChange}
             onBackstoryChange={handleBackstoryChange}
+            onTraitsChange={handleTraitsChange}
             onSubmit={handleSubmit}
           />
         </div>
@@ -64,7 +76,4 @@ const Dashboard: React.FC = () => {
   );
 };
 
-export default Dashboard;
-
-
-
+export default Dashboard2;
