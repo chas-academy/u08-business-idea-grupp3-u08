eb1cac8a01f278058833d4ca8808ddf20ecaec3e
Nejirwan <37329734+NRiCe31@users.noreply.github.com>
2023-06-05 13:20:45 +0200

    fixade responsivt

--- chatlabs/src/pages/db-4.tsx
+++ chatlabs/src/pages/db-4.tsx
@@ -1,25 +1,29 @@
 import { useParams, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { TrashIcon, PencilIcon } from '@heroicons/react/24/solid';
-
+import { useNavigate } from "react-router-dom";
+import { TrashIcon, PencilIcon, ArrowLeftIcon, ExclamationCircleIcon } from '@heroicons/react/24/solid';
 
 function DashBoard4() {
-
   const { id } = useParams();
-  const [character, setCharacter] = useState<{ name: string, backstory: string } | null>(null);
+  const [character, setCharacter] = useState<{ name: string; traits: string; backstory: string } | null>(null);
   const location = useLocation();
   const [showForm, setShowForm] = useState(false);
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate("/dashboard");
+  };
 
   useEffect(() => {
     const fetchCharacter = async () => {
       try {
         console.log(`Fetching character with: `);
-        const response = await axios.get(`http://localhost:4000/getone/${localStorage.getItem("userId")}/`+ `${location.state.index}`);
+        const response = await axios.get(`http://localhost:4000/getone/${localStorage.getItem("userId")}/${location.state.index}`);
         console.log('Response:', response.data.character);
         console.log(response.data)
         setCharacter(response.data.character);
-      } catch (error:any) {
+      } catch (error: any) {
         if (error.response && error.response.status === 404) {
           console.log('Character not found');
         } else {
@@ -28,7 +32,11 @@ function DashBoard4() {
       }
     };
 
-    
+    const animatedText = document.querySelector('.animated-text');
+    if (animatedText) {
+      animatedText.classList.add('typing-animation');
+    }
+
     fetchCharacter();
   }, [id]);
 
@@ -38,12 +46,12 @@ function DashBoard4() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({name: "Tom", backstory: "His family was murderd", traits: "Angry killer"}),
+      body: JSON.stringify({ name: "Tom", backstory: "His family was murdered", traits: "Angry killer" }),
     });
   }
-  
+
   const deleteCharacter = async (index: number) => {
-    const response = await fetch(`http://localhost:4000/delete/${localStorage.getItem("userId")}/${index}`, {method: "delete"});
+    const response = await fetch(`http://localhost:4000/delete/${localStorage.getItem("userId")}/${index}`, { method: "delete" });
     const res = await response.json();
     console.log(res);
   }
@@ -51,110 +59,102 @@ function DashBoard4() {
   if (!character) {
     return <div className='text-violet-500'>Loading...</div>;
   }
-    return (
-      <>
-        {/*Banner */}
-        <section className=" flex justify-center bg-neutral-950">
-          <div className="w-full h-full md:max-w-2xl">
-            <section className="flex flex-row justify-center my-20 md:flex-row">
-              <div className="flex flex-col justify-center mr-32">
-                <h1 className=" text-6xl font-light uppercase text-slate-100 ">
-                  <span className="text-violet-500">Dash</span>Board
-                </h1>
-              </div>
-            </section>
-          </div>
-        </section>
-  
-        <div className="relative w-full">
-          <svg
-            className="absolute inset-0 z-10 w-full -translate-y-12"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1440 320">
-            <path
-              fill="#0a0a0a"
-              fillOpacity="1"
-              d="M0,128L1440,64L1440,0L0,0Z"
-            ></path>
-          </svg>
+
+  return (
+    <>
+      {/*Banner */}
+      <section className="bg-neutral-950">
+        <div className="container mx-auto px-4 py-10">
+          <h1 className="text-4xl font-light text-center text-slate-100">
+            <span className="text-violet-500">CHARACTER</span> PROFILE
+          </h1>
         </div>
-        
+      </section>
 
-
-        {/*Card */}
-        <div className="flex justify-center pt-5 px-96 m-36 ">
-          <div className="p-5 bg-black rounded-lg h-full shadow-lg shadow-violet-500/100 ">
-            {/*Content */}
-            <div className="pt-10">
-              <h1 className="text-3xl text-violet-500 font-semibold text-center mb-40 uppercase">
-                {character.name}
-              </h1>
-            </div>
-            <div className="mx-10">
-              <h2 className="text-xl pr-10 text-violet-500 font-medium uppercase">Description:</h2>
-              <p className="text-white mt-10 mb-40">{character.backstory}</p>
-            </div>
-            {/* Buttons */}
-            <div className="flex justify-between mt">
-              <button className="px-7 py-2 bg-zinc-600 text-white font-semibold rounded-lg uppercase">Back</button>
-              <div>
-                <button onClick={() => setShowForm(!showForm)} className="px-4 py-2  bg-zinc-600 text-white font-semibold rounded-lg mr-2">
-                <PencilIcon className="h-5 w-5" /> {/* Penna Icon */}
-                </button>
-
-                <button onClick={() => deleteCharacter()} className="px-4 py-2  bg-zinc-600 text-white font-semibold rounded-lg">
-                <TrashIcon className="h-5 w-5 " /> {/* Papperskorg Icon */}
-
-                </button>
-                  
+      {/*Card */}
+      <div className="container mx-auto px-4 py-10">
+        <div className="bg-black rounded-lg shadow-lg card shadow-violet-500/100 outline outline-offset-2 outline-violet-950">
+          <div className="p-5">
+            <h1 className="mb-4 text-2xl font-semibold text-center uppercase text-violet-500">
+              {character.name}
+            </h1>
+            <div className="mb-4">
+              <h2 className="mb-2 text-lg font-medium uppercase text-violet-500">Traits:</h2>
+              <div className="flex flex-wrap">
+                {character.traits.split(',').map((trait, index) => (
+                  <span key={index} className="zoom-in flex items-center mb-2 mr-2 text-sm font-semibold text-white rounded-full bg-violet-700">
+                    <ExclamationCircleIcon className="w-4 h-4 mr-1 text-white" />
+                    <p className="text-white">{trait.trim()}</p>
+                  </span>
+                ))}
               </div>
             </div>
+            <div>
+              <h2 className="mb-2 text-lg font-medium uppercase text-violet-500">Backstory:</h2>
+              <p className="text-white">{character.backstory}</p>
+            </div>
+          </div>
+
+          {/* Buttons */}
+          <div className="flex justify-between p-5">
+            <button onClick={handleGoBack} className="flex items-center justify-center px-4 py-2 font-bold text-white rounded-lg bg-violet-700">
+              <ArrowLeftIcon className="w-5 h-5 mr-1" />
+              
+            </button>
+            <div>
+              <button onClick={() => setShowForm(!showForm)} className="flex items-center justify-center px-4 py-2 mr-2 font-semibold text-white rounded-lg bg-violet-700">
+                <PencilIcon className="w-5 h-5 mr-1" />
+                
+              </button>
+
+              <button onClick={() => deleteCharacter()} className="flex items-center justify-center px-4 py-2 font-semibold text-white rounded-lg bg-violet-700">
+                <TrashIcon className="w-5 h-5 mr-1" />
+                
+              </button>
+            </div>
           </div>
         </div>
+      </div>
 
-        <div className="flex justify-center mt-10 pt-10 mb-10">
-          <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl shadow-lg shadow-violet-500/50 hover:shadow-violet-500/100 transition duration-300">
-            { showForm &&
-              <form className=" bg-black rounded-xl shadow-lg p-10">
-                <h2 className="text-xl text-violet-500 font-semibold mb-20 mt-8 uppercase text-center">Edit Character</h2>
+      {/* Edit Form */}
+      <div className="container mx-auto px-4 py-10">
+        <div className="bg-black shadow-lg rounded-xl">
+          {showForm && (
+            <form className="p-5">
+              <h2 className="mb-4 text-lg font-semibold text-center uppercase text-violet-500">Edit Character</h2>
 
-                {/* Input fält start*/}
-                <div className="mb-2">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">
-                    Name
-                  </label>
-                  <input className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="" />
-                </div>
-                <div className="mb-2">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">
-                    Backstory
-                  </label>
-                  <input className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-20" id="username" type="text" placeholder="" />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">
-                    Traits
-                  </label>
-                  <input className="bg-black shadow appearance-none border-b rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-20" id="username" type="text" placeholder="" />
-                </div>
-                {/* Input fält Stop*/}
+              {/* Input fields */}
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-bold text-gray-700">
+                  Name
+                </label>
+                <input className="w-full px-3 py-2 mb-2 leading-tight text-gray-700 bg-black border-b rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="" />
+              </div>
+              <div className="mb-4">
+                <label className="block mb-2 text-sm font-bold text-gray-700">
+                  Backstory
+                </label>
+                <input className="w-full px-3 py-2 mb-2 leading-tight text-gray-700 bg-black border-b rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="backstory" type="text" placeholder="" />
+              </div>
+              <div className="mb-6">
+                <label className="block mb-2 text-sm font-bold text-gray-700">
+                  Traits
+                </label>
+                <input className="w-full px-3 py-2 mb-2 leading-tight text-gray-700 bg-black border-b rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="traits" type="text" placeholder="" />
+              </div>
 
-                {/* button start */}
-                <div className="flex items-center justify-between">
-                  <button onClick={() => updateCharacter()} className="bg-zinc-600 hover:bg-zinc-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline mb-10" type="button">
-                    EDIT
-                  </button>
-                </div>
-                {/* button end */}
-
-              </form>
-            }
-          </div>
+              {/* Submit button */}
+              <div className="flex justify-center">
+                <button onClick={() => updateCharacter()} className="px-4 py-2 font-bold text-white rounded-lg bg-violet-700" type="button">
+                  EDIT
+                </button>
+              </div>
+            </form>
+          )}
         </div>
-      </>
-    );
-  }
-            
-  
-  
+      </div>
+    </>
+  );
+}
+
 export default DashBoard4;
